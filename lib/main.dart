93b0194107b9a8c0ad5e2d9e9ff864dc7466417a
Xabartshik <danoshlakov12@gmail.com>
2026-03-30 20:35:11 +0900

    Тесты

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/router/app_router.dart';
 import 'core/services/auth_service.dart';
-//Сделано для диплома.
+//Сделано для диплома
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
